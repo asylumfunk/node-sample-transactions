7601810930b1f98e39410a0a630ec66885232826
asylumfunk <asylumfunk@users.noreply.github.com>
2014-03-21 12:24:50 -0700

    Fetch transaction data from API
    Note: While we gather the data/elements in a loop, we should only ever
    touch the DOM once.

--- public/assets/max/transactions.js
+++ public/assets/max/transactions.js
@@ -1,3 +1,33 @@
 $(document).ready(function () {
+	$.getJSON('/api/1/json/transactions', function (data) {
+			var response = data && data.response || {};
+			var transactions = response.transactions || [];
+			var $table = $('TABLE.transactions');
+			var $tbody = $('<TBODY>');
+			transactions.forEach(function (transaction) {
+				var date = transaction.date;
+				var to = transaction.to;
+				var amount = transaction.amount.toFixed(2);
+				$tbody.append(
+					$('<TR>')
+						.append(
+							$('<TD>')
+								.text(date)
+						)
+						.append(
+							$('<TD>')
+								.text(to)
+						)
+						.append(
+							$('<TD>')
+								.text(amount)
+						)
+				);
+			});
+			$table.append($tbody);
+	})
+	.fail(function () {
+		console.log('todo: handle failure gracefully');
+	})
 });
 
