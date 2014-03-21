cda429a0f08e1538ed351500c4e4a829937880d4
asylumfunk <asylumfunk@users.noreply.github.com>
2014-03-21 13:11:10 -0700

    Add basic validation to send money form

--- public/assets/max/send.js
+++ public/assets/max/send.js
@@ -1,5 +1,23 @@
 (function () {
 $(document).ready(function () {
+	$('FORM#send').submit(function (event) {
+		var valid = true;
+		var $to = $('#send_to');
+		var $amount = $('#send_amount');
+		if (!/\S+@\S+\.\S+/.test($to.val())) {
+			$to.addClass('error');
+			valid = false;
+		} else {
+			$to.removeClass('error');
+		}
+		if (/[^0-9,.]|^[ \t]*$/.test($amount.val())) {
+			$amount.addClass('error');
+			valid = false;
+		} else {
+			$amount.removeClass('error');
+		}
+		event.preventDefault();
+	});
 });
 }())
 
